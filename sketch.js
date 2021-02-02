47223f948591aac3a89920564572577e2f90cf3b
SebastianRojas748 <78050856+SebastianRojas748@users.noreply.github.com>
2021-02-02 15:38:30 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,37 @@
 
 // Setup code goes here
 function setup() {
-  createCanvas(600, 400);
-  textSize(40);
-  textAlign(CENTER);
+  createCanvas(1200, 800);
+  print("starting up Simple Shapes");
  }
 
 
 // Draw code goes here
 function draw() {
-  background(99);
-  fill(240,56,0);
-  text("Hello, World", width/2, height/2);
+  background(34,50,204);
+  fill(255,0,0);
+  rect(50,50,200,100);
+  stroke (210,45,67);
+  strokeWeight(6);
+
+  fill(67,33,23);
+  rect(100,300,400,120);
+
+  fill(57,34,83);
+  circle(width/2,height/2,20);
+
+fill(17,14,13);
+  ellipse(width-300, height/2, 70, 200);
+  ellipse(width-300, height/2, 300, 500);
+
+  drawRects();
+ 
+}
+function drawRects() {
+	fill(25,25,67);
+	rect(200,400,40,10);
+	rect(300,500,40,10);
+	rect(400,600,40,10);
+	rect(500,700,40,10);
+	rect(600,800,40,10);
 }
